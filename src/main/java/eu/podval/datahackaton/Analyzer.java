037a778e995d77180a13f4292254c6347a8080e3
Martin Podval <martin.podval@hp.com>
2014-03-01 17:30:38 +0100

    + sum of first 10k blocks

--- src/main/java/eu/podval/datahackaton/Analyzer.java
+++ src/main/java/eu/podval/datahackaton/Analyzer.java
@@ -7,6 +7,7 @@ import com.google.bitcoin.store.MemoryBlockStore;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.math.BigInteger;
 import java.net.InetAddress;
 import java.util.List;
 import java.util.concurrent.Future;
@@ -27,19 +28,46 @@ public class Analyzer {
             peerGroup.waitForPeers(1).get();
             Peer peer = peerGroup.getConnectedPeers().get(0);
 
+            int count = 0;
+            BigInteger sum = BigInteger.valueOf(0);
+
+            Sha256Hash blockHashToGet = new Sha256Hash("0000000099c744455f58e6c6e98b671e1bf7f37346bfd4cf5d0274ad8ee660cb");
+            while (blockHashToGet != null) {
+                Future<Block> future = peer.getBlock(blockHashToGet);
+                Block block = future.get();
+
+                for (Transaction t : block.getTransactions()) {
+                    for(TransactionOutput o : t.getOutputs()) {
+                        sum = sum.add(o.getValue());
+                        System.out.println("Value of a transaction is " + o.getValue());
+                    }
+                }
+
+                // Get previous
+                blockHashToGet = block.getPrevBlockHash();
+                count++;
+
+//                if(count % 1 == 0) {
+                    System.out.println("Processing " + count + " transaction, total amount is " + sum);
+//                }
+            }
+
+                           /*
             Sha256Hash blockHash = new Sha256Hash("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f");
             Future<Block> future = peer.getBlock(blockHash);
             Block block = future.get();
+            block.getPrevBlockHash()
+
             for (Transaction tx : block.getTransactions()) {
                 System.out.print("TX ==> " + tx);
                 for (TransactionInput i : tx.getInputs()) {
-//                    System.out.print("Input ==> " + i);
-//                    System.out.print("Value ==> " + i.);
+                    System.out.print("Input ==> " + i);
+                    System.out.print("Value ==> " + i.);
                 }
                 for (TransactionOutput o : tx.getOutputs()) {
                     System.out.print("OutputValue ==> " + o.getValue());
                 }
-            }
+            }                */
         } catch (Exception e) {
             logger.error("Processing failed.", e);
         } finally {
